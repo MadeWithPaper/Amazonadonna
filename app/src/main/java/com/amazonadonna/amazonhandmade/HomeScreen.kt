800c0216eb0c4c24fec8a9579e835194b0ced1e8
Jacky Huang <jhuang81@calpoly.edu>
2018-11-13 14:14:05 -0800

    added 2 UI screens, layout

--- app/src/main/java/com/amazonadonna/amazonhandmade/HomeScreen.kt
+++ app/src/main/java/com/amazonadonna/amazonhandmade/HomeScreen.kt
@@ -1,12 +1,44 @@
 package com.amazonadonna.amazonhandmade
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home_screen.*
 
 class HomeScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        //List All Artisan button
+        listAllArtisan.setOnClickListener{
+            queryAllArtisan()
+        }
+
+        addArtisan.setOnClickListener{
+            addSingleArtisan()
+        }
+    }
+
+
+    private fun queryAllArtisan() {
+        //TODO add funtion to do GET reqest to back end
+
+        //starts new screen
+        val intent = Intent(this, ListAllArtisans::class.java)
+        // To pass any data to next activity
+        //OPTIONAL: intent.putExtra("keyIdentifier", value)
+        // start your next activity
+        startActivity(intent)
+    }
+
+    private fun addSingleArtisan() {
+        //starts new screen
+        val intent = Intent(this, AddArtisan::class.java)
+        // To pass any data to next activity
+        //OPTIONAL: intent.putExtra("keyIdentifier", value)
+        // start your next activity
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/amazonadonna/amazonhandmade/ListAllArtisans.kt
@@ -0,0 +1,16 @@
+package com.amazonadonna.amazonhandmade
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+
+
+class ListAllArtisans : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_all_artisans)
+
+    }
+
+}
