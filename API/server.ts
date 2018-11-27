55e08d0789149239342c75a3589c80a5e9a32612
lmschmid <liam.schmid1212@gmail.com>
2018-11-27 13:07:39 -0800

    Added aws-sdk and DynamoDB instance

--- API/server.ts
+++ API/server.ts
@@ -1,13 +1,19 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+// Look into amazon sdk
+
 import * as path from 'path'
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as aws from 'aws-sdk'
 
 const app = express()
 const port = process.env.PORT || 3000
 const dev = process.env.PROD === 'false'
+
+aws.config.update({region: 'us-west-2'})
+const ddb = new aws.DynamoDB({apiVersion: '2012-10-08'})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
