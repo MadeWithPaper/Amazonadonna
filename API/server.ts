0a7a581ab4fb0ec63deb9d4fd7b7224e3be53593
lmschmid <liam.schmid1212@gmail.com>
2018-11-27 13:51:24 -0800

    Added dumb addArtisan route

--- API/server.ts
+++ API/server.ts
@@ -1,8 +1,6 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-// Look into amazon sdk
-
 import * as path from 'path'
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
@@ -12,8 +10,8 @@ const app = express()
 const port = process.env.PORT || 3000
 const dev = process.env.PROD === 'false'
 
-aws.config.update({region: 'us-west-2'})
-const ddb = new aws.DynamoDB({apiVersion: '2012-10-08'})
+aws.config.update({ region: 'us-east-1' })
+const ddb = new aws.DynamoDB({ apiVersion: '2012-10-08' })
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -24,6 +22,13 @@ app.listen(port, () => {
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.send('I did it!')
+    ddb.listTables({ Limit: 10 }, (err, data) => {
+        if (err) {
+            console.log('Error', err.code)
+        } else {
+            console.log('Table names are ', data.TableNames)
+        }
+    })
     // res.sendFile(path.resolve(__dirname, 'frontEnd', 'index.html'))
 })
 
@@ -31,9 +36,32 @@ app.get('/getDatabase', (req: express.Request, res: express.Response) => {
     res.json({ name: 'Mitchell' })
 })
 
-app.post('/addToDatabase', (req: express.Request, res: express.Response) => {
-    console.log(req.body)
-    res.send()
-})
+const params: aws.DynamoDB.PutItemInput = {
+    TableName: 'artisan',
+    Item: {
+        artisanId: { S: '1928485' },
+        cgoId: { S: '90218495' },
+        bio: { S: 'Hi I am artisan' },
+        city: { S: 'Tijuana' },
+        country: { S: 'MX' },
+        name: { S: 'Jose Calderon' },
+        lat: { N: '32.5149' },
+        long: { N: '117.0382' }
+    }
+}
+app.post(
+    '/addArtisanToDatabase',
+    (req: express.Request, res: express.Response) => {
+        console.log(req.body)
+        res.send()
+        ddb.putItem(params, (err, data) => {
+            if (err) {
+                console.log('Error', err.code)
+            } else {
+                console.log('Attributes ', data)
+            }
+        })
+    }
+)
 
 // app.use(express.static(path.resolve(__dirname, 'frontEnd')))
