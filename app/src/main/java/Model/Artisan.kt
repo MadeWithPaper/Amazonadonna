08b4e4c21a5ea9508df104a43673148747842982
Jacky Huang <jhuang81@calpoly.edu>
2018-11-29 12:33:28 -0800

    modified readme, added ui stub, workoing on adding data class

--- app/src/main/java/Model/Artisan.kt
+++ app/src/main/java/Model/Artisan.kt
@@ -1,13 +1,28 @@
-class Artisan (var firstName : String, var lastName : String, var community : String, var artisanBio : String) {
-    var artisanID : String = ""
+import com.beust.klaxon.Json
 
-    init {
-        generateArtisanID()
-    }
+class Artisan (val artisanID : String){
+
+//    @Json(artisanId = "artisanId")
+//    val artisanID : String,
+//    @Json(name = "name")
+//    val artisanName : String,
+
+
+
+
+//          JSON data sample
+//        { city: { S: ‘CityName’ },
+//            bio: { S: ’Hi I\‘m Juan.’ },
+//            lon: { N: ‘0’ },
+//            artisanId: { S: ‘F5SJ72’ },
+//            lat: { N: ‘0’ },
+//            country: { S: ‘MX’ },
+//            name: { S: ‘Juan Gonzalez’ },
+//            cgoId: { S: ‘0’ } }
+
 
     private fun generateArtisanID() {
         //TODO fill in logic for generating unique ID for artisan
-       artisanID = "x"
     }
 
 
--- app/src/main/java/com/amazonadonna/amazonhandmade/ListAllArtisans.kt
+++ app/src/main/java/com/amazonadonna/amazonhandmade/ListAllArtisans.kt
@@ -3,6 +3,7 @@ package com.amazonadonna.amazonhandmade
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import java.net.URL
 
 
 class ListAllArtisans : AppCompatActivity() {
@@ -10,6 +11,17 @@ class ListAllArtisans : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_all_artisans)
+        val result = URL("").readText()
+
+//          JSON data sample
+//        { city: { S: ‘CityName’ },
+//            bio: { S: ’Hi I\‘m Juan.’ },
+//            lon: { N: ‘0’ },
+//            artisanId: { S: ‘F5SJ72’ },
+//            lat: { N: ‘0’ },
+//            country: { S: ‘MX’ },
+//            name: { S: ‘Juan Gonzalez’ },
+//            cgoId: { S: ‘0’ } }
 
     }
 
