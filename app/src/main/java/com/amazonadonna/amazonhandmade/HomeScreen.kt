631731ed4e9e9fd8bb9579994306836c70125168
Jaz Singh <jbsingh55@gmail.com>
2018-12-02 17:49:50 -0800

    added a post, just need to parse user info

--- app/src/main/java/com/amazonadonna/amazonhandmade/HomeScreen.kt
+++ app/src/main/java/com/amazonadonna/amazonhandmade/HomeScreen.kt
@@ -53,10 +53,51 @@ class HomeScreen : AppCompatActivity() {
 
     private fun addSingleArtisan() {
         //starts new screen
-        val intent = Intent(this, AddArtisan::class.java)
+        //val intent = Intent(this, AddArtisan::class.java)
         // To pass any data to next activity
         //OPTIONAL: intent.putExtra("keyIdentifier", value)
         // start your next activity
-        startActivity(intent)
+        //startActivity(intent)
+//        {
+//            "artisanId":"56145",
+//            "cgoId": "0",
+//            "bio": "Hi I make necklaces",
+//            "city": "Tijuana",
+//            "country": "MX",
+//            "name": "Mitchell",
+//            "lat": "32.5149",
+//            "lon": "117.0382"
+//        }
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+        val requestBody = FormBody.Builder().add("artisanId","parse_artisanId")
+                .add("cgoId","parse_cgoID")
+                .add("bio", "parse_bio")
+                .add("city","parse_city")
+                .add("country", "parse_country")
+                .add("name", "parse_name")
+                .add("lat", "parse_lat")
+                .add("lon", "parse_lon")
+                .build()
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body) //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute request") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
+
+
+
     }
 }
