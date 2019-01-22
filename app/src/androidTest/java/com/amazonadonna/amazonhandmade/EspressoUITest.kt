e111ed466067457ab9d3f4a90b896e5c82e8ec12
Jacky <huangjacky.1996@gmail.com>
2019-01-22 13:17:31 -0800

    ui test setup run#14

--- app/src/androidTest/java/com/amazonadonna/amazonhandmade/EspressoUITest.kt
+++ app/src/androidTest/java/com/amazonadonna/amazonhandmade/EspressoUITest.kt
@@ -2,30 +2,26 @@ package com.amazonadonna.amazonhandmade
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.junit.Test
-import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 
 class EspressoUITest {
 
     @get: Rule
-    var activityRule : ActivityTestRule<LoginScreen> = ActivityTestRule(LoginScreen::class.java)
+    var activityRule : IntentsTestRule<LoginScreen> = IntentsTestRule(LoginScreen::class.java)
 
 
     @Test
     fun login_button_pressed() {
 
-        //TODO add false emails and passwords
-        Intents.init()
         onView(withId(R.id.email_sign_in_button)).perform(click())
-        println("RUNNING A TEST IN UI")
+        println("RUNNING THE TEST")
         intended(hasComponent(HomeScreen::class.java.getName()))
-        Intents.release()
-    }
 
+    }
 
 }
