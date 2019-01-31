38abf91a30ddabe1c91438f7ab803e8152900952
Jaz Singh <jbsingh55@gmail.com>
2019-01-31 12:47:35 -0800

    added product and order ID generator

--- app/src/main/java/com/amazonadonna/model/Order.kt
+++ app/src/main/java/com/amazonadonna/model/Order.kt
@@ -9,11 +9,17 @@ import java.util.*
 
 @Entity(tableName = "order")
 data class Order (
-        @ColumnInfo(name = "shippingAddress") @Json(name = "shippingAddress") var shippingAddress : Double,
+        @ColumnInfo(name = "shippingAddress") @Json(name = "shippingAddress") var shippingAddress : String,
         @PrimaryKey @Json(name = "orderId") var orderId : String,
         @ColumnInfo(name = "totalCost") @Json(name = "totalCost") var totalCost : Double,
         @ColumnInfo(name = "products") @Json(name = "products") var items : List<Product>,
         @ColumnInfo(name = "cgaId") @Json(name = "cgaId") var cgaId : String): Serializable {
+
+    fun generateOrderID() {
+        //TODO fill in logic for generating unique ID for artisan
+        var num = Random().nextInt()
+        orderId = shippingAddress + cgaId + num.toString()
+    }
 
 
 
