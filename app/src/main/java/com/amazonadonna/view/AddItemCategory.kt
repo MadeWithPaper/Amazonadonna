049500d54d52b1867eb014c082b53ab5d575060b
Jacky <huangjacky.1996@gmail.com>
2019-02-01 21:34:01 -0800

    all hard coded categories are in, need to add a 3rd spinner

--- app/src/main/java/com/amazonadonna/view/AddItemCategory.kt
+++ app/src/main/java/com/amazonadonna/view/AddItemCategory.kt
@@ -12,7 +12,7 @@ import java.util.*
 class AddItemCategory : AppCompatActivity() {
 
     //array of values for each adapter
-    val mainCategoryList = arrayOf("Jewelry", "Home & Kitchen", "Clothing, Shoes & Accessories", "Wedding")
+    val mainCategoryList = arrayOf("Jewelry", "Home & Kitchen", "Clothing, Shoes & Accessories", "Wedding", "Handbags & Totes", "Beauty & Grooming", "Stationery & Party Supplies", "Toys & Games", "Pet Supplies", "Sports & Outdoors", "Baby")
     val jewelrysub = arrayOf("Accessories", "Anklets", "Body Jewelry", "Bracelets", "Brooches & Pins", "Charms", "Cufflings & Shirt Accessories", "Earrings", "Hair Jewelry", "Jewelry Sets", "Necklaces", "Pendants & Coins", "Rings", "Wedding & Engagement")
     val accessoriessub = arrayOf("Jewelry Boxes & Organizers", "Keychains & Keyrings")
     val nosub = arrayOf("")
@@ -33,9 +33,49 @@ class AddItemCategory : AppCompatActivity() {
     val lawngardensub = arrayOf("Backyard Birding", "Outdoor Decor")
     val storageorganizationsub = arrayOf("Baskets & Bins", "Bathroom Storage & Organization", "Beauty & Personal Care Storage & Organization", "Clothing & Wardrobe Storage", "Home Stroage Hooks", "Jewelry Boxes & Organizers", "Kitchen Storage & Organization", "Office Storage & Organization", "Racks & Shelves", "Ring Bearer Pillows", "Storage Bags", "Wedding Dress Hangers")
     val cleanningsupsub = arrayOf("Cleaning Tools", "Dishwashing", "Laundry")
-    val csasub = arrayOf("Women", "Men", "Girls", "Boys", "baby", "Hair Accessories", "Handbags & Shoulder Bags", "Insoles & Shoe Accessories", "Luggage & Travel Gear")
-    //TODO sub of csa
     val weddingsub = arrayOf("Fashion Accessories", "Gifts & Keepsakes", "Handbags & Pocket Accessories", "Jewelry & Jewelry Accessories", "Stationery & Party Supplies", "Wedding Decor")
+    val fashionaccessoriessub = arrayOf("Garters", "Gloves", "Hair Accessories", "Hats & Veils", "Scarves, Shawls & Sashes", "Ties", "Wedding Dress Belts")
+    val giftskeepsakessub = arrayOf("Bar Accessories", "Cups & Mugs", "Cutting Boards", "Gift Baskets", "Gift Tags", "Glassware", "Guestbooks", "Photo Albums", "Photo Frames", "Posters & Prints", "Wedding Dress Hangers")
+    val weddingaccessoriessub = arrayOf("Clutches", "Totes", "Wristlets", "Coin Purses & Pouches", "Keychains & Keyrings", "Money Clips", "Pocket Squares")
+    val weddingjewelrysub = arrayOf("Anklets", "Bracelets", "Brooches & Pins", "Charms", "Cufflinks & Shirt Accessories", "Earrings", "Jewelry Sets", "Necklaces", "Ring Bearer Pillows", "Ring Boxes", "Wedding & Engagement Rings")
+    val weddingstationerysub = arrayOf("Party Supplies", "Stationery")
+    val weddingdecorsub = arrayOf("Aisle Runners", "Artificial Flora", "Candles & Holders", "Centerpieces", "Confetti", "Decorative Boxes", "Dining & Tableware", "Garlands & Banners", "Jars", "Place Cards", "Ring Bearer Pillows", "Ring Boxes", "Signs", "Table Numbers", "Vases")
+    val csasub = arrayOf("Women", "Men", "Girls", "Boys", "baby", "Hair Accessories", "Handbags & Shoulder Bags", "Insoles & Shoe Accessories", "Luggage & Travel Gear")
+    val womenandmensub = arrayOf("Clothing", "Shoes", "Accessories", "Watches")
+    val girlboysub = arrayOf("Clothing", "Shoes", "Accessories")
+    val babygendersub = arrayOf("Baby Girls", "Baby Boys")
+    val hairaccessoriessub = arrayOf("Bun Shapers", "Clips & Barrettes", "Hair Extensions & Wigs", "Hair Pins", "Hair Ties & Elastics", "Headbands", "Side Combs", "Tiaras")
+    val csabags = arrayOf("Backpack Handbags", "Clutches", "Cross-Body Bags", "Satchels", "Totes", "Wristlets")
+    val luggagesub = arrayOf("Bags", "Luggage", "Travel Accessories", "Wallets, Identification & Bag", "Accessories")
+    val handbagssub = arrayOf("Backpack Handbags", "Clutches", "Cross-Body Bags", "Satchels", "Totes", "Wristlets")
+    val groomingsub = arrayOf("Baby & Child Care", "Fragrance", "Hair Care", "Makeup", "Personal Care", "Shaving & Hair Removal", "Skin Care", "Tools & Accessories", "Wellness & Relaxation")
+    val childcaresub = arrayOf("Bath", "Diaper Care")
+    val fragrancesub = arrayOf("Men", "Women")
+    val haircaresub = arrayOf("Hair & Scalp Care", "Hair Accessories", "Shampoo", "Styling Products")
+    val makeupsub = arrayOf("Eyes", "Face", "Lips", "Nails")
+    val personalcaresub = arrayOf("Bath & Bathing Accessories", "Deodorants & Antiperspirants")
+    val hairremovalsub = arrayOf("Aftershaves", "Beard & Mustache Care", "Razors & Blades", "Shaving & Grooming Sets", "Shaving Accessories", "Shaving Creams, Lotions & Gels")
+    val skincaresub = arrayOf("Body", "Eyes", "Face", "Feet, Hands & Nails", "Lip Care", "Sets & Kits")
+    val beautytoolssb = arrayOf("Bathing Accessories", "Feet, Hand & Nail Tools", "Mirrors & Magnifiers", "Shaving & Hair Removal Tools", "Storage & Organization")
+    val stationerypartysub = arrayOf("Party Supplies", "Pens & Pencils", "Stationery")
+    val partysuppliessub = arrayOf("Aisle Runners", "Centerpieces", "Ceremony Programs", "Confetti", "Decorations", "Garlands & Decorative Banners", "Menu Cards", "Party Favors", "Party Hats & Masks", "Place Cards", "Table Numbers")
+    val stationerysub = arrayOf("Appointment Books & Planners", "Gift Tags", "Gift Wrapping Paper", "Invitations", "Labels", "Notecards & Greeting Cards", "Paper", "Paper Stationery", "Photo Albums", "Stamps", "Stickers", "Tank You Cards", "Wall Calendars")
+    val toysgamessub = arrayOf("Baby & Toddler Toys", "Dolls, Toy Figures & Accessories", "Lawn & Playground", "Learning & Education", "Musical Toy Instruments", "Novelty & Gag Toys", "Plushies & Stuffed Animals", "Pretend Play", "Puppets", "Puzzles")
+    val noveltygagtoysub = arrayOf("Magnets & Magnetic Toys", "Money Banks", "Temporary Tattoos")
+    val petsuppliessub = arrayOf("Dogs", "Cats")
+    val dogsub = arrayOf("Apparel & Accessories", "Beds & Furniture", "Collars, Harnesses & Leashes", "Feeding & Watering Supplies", "Litter & Housebreaking", "Storage & Organization", "Toys", "Treats", "Memorials & Ums")
+    val catsub = arrayOf("Apparel & Accessories", "Beds & Furniture", "Collars, Harnesses & Leashes", "Feeding & Watering Supplies", "Toys")
+    val sportsgoodssub = arrayOf("Camping & Hiking", "Car & Vehicle Accessories", "Cycling", "Fishing", "Hunting & Shooting", "Sports & Fitness")
+    val campingsub = arrayOf("Axes", "Fire Starters", "Flasks", "Knives", "Lighters", "Survival Bracelets", "Walking Sticks")
+    val vehicleaccessoriessub = arrayOf("Decals", "Door Straps & Grab Handles", "Gold Cart Seats & Covers", "Hitch Covers", "Plates", "Tire Covers")
+    val huntingsub = arrayOf("Archery", "Gun Accessories", "Knives & Accessories")
+    val fitnesssub = arrayOf("Accessories", "Golf", "Skateboarding", "Yoga")
+    val babysub = arrayOf("Baby & Toddler Toys", "Baby Clothing, Shoes & Accessories", "Child Carriers", "Diaper Changing", "Nursery Bedding", "Nursery Decor", "Nursery Furniture", "Nursing & Feeding", "Pacifiers & Teethers")
+    val diapersuppliessub = arrayOf("Changing Pads & Covers", "Diaper Bags", "Diapers", "Pails, Liners & Bags")
+    val nurserybeddingsub = arrayOf("Back & Body Supports", "Blankets & Swaddling", "Crib Bedding", "Pillow Protectors", "Pillowcases", "Pillows", "Playard Bedding", "Quilts & Bed Covers")
+    val nurserydecorsub = arrayOf("Clocks", "Framed Pictures", "Height Charts", "Light Switch Decorations", "Mobiles", "Night Lights & Night Light", "Covers", "Wall Letters & Numbers", "Wall Stickers")
+    val nursingfeedingsub = arrayOf("Bibs & Burp Cloths", "Drinkware")
+    val pacifierssub = arrayOf("Pacifier Accessories", "Pacifiers", "Teethers")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +88,13 @@ class AddItemCategory : AppCompatActivity() {
         val homeSubArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, homeKitchensub)
         val csaSubArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, csasub)
         val weddingSubArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, weddingsub)
+        val handbagstotesSubArrayAdapter = ArrayAdapter (this, android.R.layout.simple_spinner_item, handbagssub)
+        val beautySubArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, groomingsub)
+        val stationerySubArrayAdapter = ArrayAdapter (this, android.R.layout.simple_spinner_item, stationerypartysub)
+        val toysgamesSubArrayAdapter = ArrayAdapter (this, android.R.layout.simple_spinner_item, toysgamessub)
+        val petsuppliesSubArrayAdapter = ArrayAdapter (this, android.R.layout.simple_spinner_item, petsuppliessub)
+        val sportsgoodsSubArrayAdapter = ArrayAdapter (this, android.R.layout.simple_spinner_item, sportsgoodssub)
+        val babySubArrayAdapter = ArrayAdapter (this, android.R.layout.simple_spinner_item, babysub)
         val placeholderArrayAdapter = ArrayAdapter (this, android.R.layout.simple_spinner_item, weddingsub)
         // Set layout to use when the list of choices appear
         mainArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -74,14 +121,32 @@ class AddItemCategory : AppCompatActivity() {
                 } else if (spinnerValue.equals("Wedding")) {
                     weddingSubArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     itemCategory_subSpinner.adapter = weddingSubArrayAdapter
+                } else if (spinnerValue.equals("Handbags & Totes")) {
+                    handbagstotesSubArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    itemCategory_subSpinner.adapter = handbagstotesSubArrayAdapter
+                } else if (spinnerValue.equals("Beauty & Grooming")) {
+                    beautySubArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    itemCategory_subSpinner.adapter = beautySubArrayAdapter
+                } else if (spinnerValue.equals("Stationery & Party Supplies")) {
+                    stationerySubArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    itemCategory_subSpinner.adapter = stationerySubArrayAdapter
+                } else if (spinnerValue.equals("Toys & Games")) {
+                    toysgamesSubArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    itemCategory_subSpinner.adapter = toysgamesSubArrayAdapter
+                } else if (spinnerValue.equals("Pet Supplies")) {
+                    petsuppliesSubArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    itemCategory_subSpinner.adapter = petsuppliesSubArrayAdapter
+                } else if (spinnerValue.equals("Sports & Outdoors")) {
+                    sportsgoodsSubArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    itemCategory_subSpinner.adapter = sportsgoodsSubArrayAdapter
+                } else if (spinnerValue.equals("Baby")) {
+                    babySubArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    itemCategory_subSpinner.adapter = babySubArrayAdapter
                 } else {
                     placeholderArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     itemCategory_subSpinner.adapter = placeholderArrayAdapter
                 }
             }
-
         }
-
-
     }
 }
