6deb81ff7232541c2635d6c53963a8765991ebb2
Jacky <huangjacky.1996@gmail.com>
2019-02-04 01:35:27 -0800

    refactor spinner code

---
+++ app/src/main/java/com/amazonadonna/amazonhandmade/AddItemInfo.kt
@@ -0,0 +1,13 @@
+package com.amazonadonna.amazonhandmade
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.amazonadonna.view.R
+
+class AddItemInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item_info)
+    }
+}
