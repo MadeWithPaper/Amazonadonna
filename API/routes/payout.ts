9e1e0ea3f4da5cad66c9cbab8c7dda547c9835fa
Victor Wei <vywei@Victors-MacBook.local>
2019-02-20 17:27:38 -0800

    payouts

---
+++ API/routes/payout.ts
@@ -0,0 +1,120 @@
+import { Router, Request, Response } from 'express'
+import * as aws from 'aws-sdk'
+import * as multer from 'multer'
+import * as multerS3 from 'multer-s3'
+import * as mime from 'mime'
+import { ddb, s3 } from '../server'
+import { Payout } from '../models/payout'
+import { unmarshUtil } from '../utilities/unmarshall'
+
+const router = Router()
+
+router.post('/listAllForArtisan', (req: Request, res: Response) => {
+    const listAllPayoutsParams: aws.DynamoDB.Types.QueryInput = {
+        TableName: 'payout',
+        IndexName: 'artisanId-index',
+        KeyConditionExpression: 'artisanId = :id',
+        ExpressionAttributeValues: {
+            ':id': { S: req.body.artisanId }
+        }
+    }
+    ddb.query(listAllPayoutsParams, (err, data) => {
+        if (err) {
+            const msg = 'Error fetching payots in payout/listAllForArtisan'
+            console.log(msg + err)
+            res.status(400).send(msg + err.message)
+        } else {
+            const convert = unmarshUtil(data.Items)
+            Promise.all(convert).then(items => {
+                res.json(items)
+            })
+        }
+    })
+})
+
+router.post('/add', (req: Request, res: Response) => {
+    const params: aws.DynamoDB.PutItemInput = {
+        TableName: 'payout',
+        Item: {
+            payoutId: { S: req.body.payoutId },
+            artisanId: { S: req.body.artisanId },
+            cgoId: { S: req.body.cgoId },
+            amount: { N: req.body.numItems },
+            /*Date values are stored as ISO-8601 formatted strings*/
+            date: { S: req.body.date },
+            signaturePicURL: { S: 'Not set' }
+        }
+    }
+    ddb.putItem(params, (err, data) => {
+        if (err) {
+            console.log('Error adding order in payout/add: ', err)
+            res.status(400).send(
+                'Error adding order in payout/add: ' + err.message
+            )
+        } else {
+            res.send('Successfully added')
+        }
+    })
+})
+
+router.post('/updateImage', (req: Request, res: Response) => {
+    // setup pic uploader with payoutId as filename
+    const PayoutSignatureUploader = multer({
+        storage: multerS3({
+            s3,
+            bucket: 'payout-signatures',
+            acl: 'public-read',
+            contentType: (picReq, file, cb) => {
+                cb(null, file.mimetype)
+            },
+            metadata: (picReq, file, cb) => {
+                cb(null, { fieldName: file.fieldname })
+            },
+            key: (picReq, file, cb) => {
+                cb(
+                    null,
+                    req.body.payoutId + '.' + mime.getExtension(file.mimetype)
+                )
+            }
+        })
+    })
+
+    const singleSignatureUpload = PayoutSignatureUploader.single('image')
+
+    // upload pic
+    singleSignatureUpload(req, res, picErr => {
+        if (picErr) {
+            const msg = 'Error uploading picture in payout/updateImage'
+            console.log(msg, picErr)
+            res.status(422).send(msg + picErr.message)
+        } else {
+            let signaturePicURL = 'Error: no picture attached'
+            if (req.file) {
+                signaturePicURL = (req.file as any).location
+            }
+
+            // update db record with new URL
+            const params: aws.DynamoDB.UpdateItemInput = {
+                TableName: 'payout',
+                Key: { payoutId: { S: req.body.payoutId } },
+                UpdateExpression: 'set signaturePicURL = :u',
+                ExpressionAttributeValues: { ':u': { S: signaturePicURL } },
+                ReturnValues: 'UPDATED_NEW'
+            }
+            // check string, params
+            ddb.updateItem(params, (err, data) => {
+                if (err) {
+                    const msg = 'Error updating payout record '
+                    console.log(msg + req.body.payoutId + ' : ' + err)
+                    res.status(400).send(
+                        msg + req.body.payoutId + ' : ' + err.message
+                    )
+                } else {
+                    res.json({ imageUrl: signaturePicURL })
+                }
+            })
+        }
+    })
+})
+
+export { router as payoutRouter }
