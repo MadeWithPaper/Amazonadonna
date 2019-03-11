9680e2aed49e9b800ecf793895f46c8619381bfe
Jacky <huangjacky.1996@gmail.com>
2019-03-10 17:44:54 -0700

    reports ui done, needs logic

---
+++ app/src/main/java/com/amazonadonna/view/ReportTargetAdapter.kt
@@ -0,0 +1,49 @@
+package com.amazonadonna.view
+
+import android.content.Context
+import android.graphics.Color
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.amazonadonna.model.ReportTarget
+import kotlinx.android.synthetic.main.report_target_cell.view.*
+
+
+class ReportTargetAdapter (private val context: Context, private val targets : List<ReportTarget>) : RecyclerView.Adapter<TargetViewHolder> () {
+
+    private val TAG = "ReportTargetAdapter"
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TargetViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cellForRow = layoutInflater.inflate(R.layout.report_target_cell, parent, false)
+        return TargetViewHolder(cellForRow)
+    }
+
+    override fun getItemCount(): Int {
+        return targets.count()
+    }
+
+    override fun onBindViewHolder(holder: TargetViewHolder, position: Int) {
+        val target = targets[position]
+        holder.bindOrder(target)
+        //holder.view.setBackgroundColor(if (target.selected) Color.GREEN else Color.WHITE)
+        //holder.view.setBackgroundColor(Color.GREEN)
+        holder.view.setOnClickListener{
+            //notifyItemChanged(position)
+            target.selected = !target.selected
+            //holder.view.setBackgroundColor(if (target.selected) Color.GREEN else Color.WHITE)
+            Log.i(TAG, "clicked ${target.selected}")
+            notifyItemChanged(position)
+        }
+    }
+
+}
+
+class TargetViewHolder (val view : View) : RecyclerView.ViewHolder(view) {
+    fun bindOrder(target : ReportTarget) {
+        view.targetNameTV.text = target.artisan.artisanName
+        //view.setBackgroundColor(Color.RED)
+        view.setBackgroundColor(if (target.selected) Color.GREEN else Color.WHITE)
+    }
+}
