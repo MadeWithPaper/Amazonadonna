bec9a9a2c502b15fba023b18f047a46bad472989
Mitchell <mover333@gmail.com>
2019-05-14 19:29:32 -0700

    not delteing item

--- API/utilities/active.ts
+++ API/utilities/active.ts
@@ -1,7 +1,9 @@
 import * as _ from 'lodash'
 
 const filterActive = (items: any) => {
-    return _.filter(items, { active: 'true' })
+    return _.filter(items, (o: any) => {
+        return o.active === undefined || o.active === 'true'
+    })
 }
 
 export { filterActive }
