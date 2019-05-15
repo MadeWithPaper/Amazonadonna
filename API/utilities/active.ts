c00672100011a24061eae231523617247e381436
Mitchell <mover333@gmail.com>
2019-05-15 14:19:27 -0700

    fixing delete

--- API/utilities/active.ts
+++ API/utilities/active.ts
@@ -1,9 +1,17 @@
 import * as _ from 'lodash'
 
 const filterActive = (items: any) => {
-    return _.filter(items, (o: any) => {
-        return o.active === undefined || o.active === 'true'
-    })
+    if (items.length && items.length > 1) {
+        return _.filter(items, (o: any) => {
+            return o.active === undefined || o.active === 'true'
+        })
+    } else {
+        if (items.active === 'true') {
+            return items
+        } else {
+            return {}
+        }
+    }
 }
 
 export { filterActive }
